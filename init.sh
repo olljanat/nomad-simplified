7b34df1ff8aaf436303bf8315426c53d4206ec22
Olli Janatuinen <olli.janatuinen@gmail.com>
2024-11-10 10:36:39 +0000

    Cert draft

--- init.sh
+++ init.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+
+if [ ! -f "certs/nomad-simplified.crt" ]; then
+  openssl req -x509 -newkey rsa:4096 -sha256 -days 1 \
+    -nodes -keyout certs/all-in-one.key -out certs/all-in-one.crt -subj "/CN=nomad-simplified.local" \
+    -addext "subjectAltName=DNS:nomad-simplified.local,DNS:node1.nomad-simplified.local,DNS:node2.nomad-simplified.local,DNS:node3.nomad-simplified.local,IP:127.0.0.1"
+fi
+
+if [ ! -f "certs/rootca.crt" ]; then
+  openssl genrsa -out certs/rootca.key 4096
+  openssl req -new -x509 -days 10950 -key certs/rootca.key -out certs/rootca.crt -config certs/rootca.cnf
+fi
+
+if [ ! -f "certs/node.crt" ]; then
+  openssl genrsa -out certs/node.key 4096
+  openssl req -new -key certs/node.key -out certs/node.csr -config certs/node.cnf 
+  openssl ca -in certs/node.csr -out certs/node.crt -days 90 -notext -batch -config certs/openssl.cnf
+fi
+
 if [ ! -d "vault/data" ]; then
   openssl req -x509 -newkey rsa:4096 -sha256 -days 1 \
     -nodes -keyout vault/init-config/vault-init.key -out vault/init-config/vault-init.crt -subj "/CN=localhost" \

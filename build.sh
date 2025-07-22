141d50811580322817c3e65863db6570665c9419
Olli Janatuinen <olli.janatuinen@gmail.com>
2025-07-22 12:28:49 +0000

    First version

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -euxo pipefail
+
+org=$1
+repo=$2
+nomad_version=$3
+coredns_nomad_version=$4
+version=$5
+
+# Build Docker images for Linux
+TAG="$org/$repo:$version"
+docker build . -t $TAG \
+  --build-arg NOMAD_VERSION=$nomad_version \
+  --build-arg COREDNS_NOMAD_VERSION=$coredns_nomad_version
+docker push $TAG
+
+# Build ZIP files for Windows
+mkdir -p dist/tmp
+wget https://github.com/olljanat/nomad/releases/download/v1.10.3-olljanat1/nomad_windows_amd64.zip -O dist/nomad.zip
+unzip -o dist/nomad.zip -d dist/tmp/bin/
+rm dist/nomad.zip
+
+# Include configs
+mkdir -p dist/tmp/etc/nomad.d dist/tmp/opt/nomad/data dist/tmp/logs dist/tmp/opt/tls
+cp -r nomad.d/* dist/tmp/etc/nomad.d/
+
+# Remove Linux specific files
+rm -f dist/tmp/etc/nomad.d/linux.hcl
+rm -rf dist/tmp/etc/nomad.d/role
+
+# Create ZIP
+cd dist/tmp
+zip -r ../nomad-windows.zip .
+cd ../..
+rm -rf dist/tmp
---
+++ nomad.d/role/server.hcl
@@ -0,0 +1,18 @@
+acl {
+  token_max_expiration_ttl = "720h"
+}
+
+client {
+  enabled = false
+}
+
+server {
+  enabled = true
+  bootstrap_expect = 3
+}
+
+tls {
+  ca_file = "/opt/tls/nomad-agent-ca.pem"
+  cert_file = "/opt/tls/nomad-server.pem"
+  key_file = "/opt/tls/nomad-server-key.pem"
+}

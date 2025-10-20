7164a06f327e5cf54338ed9438843ef9bf6f9bab
Olli Janatuinen <olli.janatuinen@gmail.com>
2025-10-20 09:54:42 +0000

    Nomad v1.11.0-beta.1 and CoreDNS v1.13.1

--- build.sh
+++ build.sh
@@ -4,19 +4,19 @@ set -euxo pipefail
 org=$1
 repo=$2
 nomad_version=$3
-coredns_nomad_version=$4
+coredns_version=$4
 version=$5
 
 # Build Docker images for Linux
 TAG="$org/$repo:$version"
 docker build . -t $TAG \
   --build-arg NOMAD_VERSION=$nomad_version \
-  --build-arg COREDNS_NOMAD_VERSION=$coredns_nomad_version
+  --build-arg COREDNS_VERSION=$coredns_version
 docker push $TAG
 
 # Build ZIP files for Windows
 mkdir -p dist/tmp
-wget https://github.com/olljanat/nomad/releases/download/${nomad_version}/windows_amd64.zip -O dist/nomad.zip
+wget https://releases.hashicorp.com/nomad/${nomad_version}/nomad_${nomad_version}_windows_amd64.zip -O dist/nomad.zip
 unzip -o dist/nomad.zip -d dist/tmp/bin/
 rm dist/nomad.zip
 
